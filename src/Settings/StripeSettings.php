12108d832d3ee2a9d583e1deaf766227402a3d35
Andrew Cuthbert <acuthbert@gcdtech.com>
2019-08-01 12:55:38 +0100

    Added settings class

---
+++ src/Settings/StripeSettings.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Gcd\Scaffold\Payments\Stripe\Settings;
+
+use Rhubarb\Crown\Settings;
+
+class StripeSettings extends Settings
+{
+    public $secretKey;
+
+    public $publicKey;
+}
