6d8fc7f0ca9c80f911c324c0bd513e7687d590b2
jmcelreavey <jmcelreavey@gcdtech.com>
2019-08-05 11:16:19 +0100

    Stripe service storing changes

--- src/Services/StripePaymentService.php
+++ src/Services/StripePaymentService.php
@@ -5,6 +5,8 @@ namespace Gcd\Scaffold\Payments\Stripe\Services;
 use Gcd\Scaffold\Payments\Services\PaymentService;
 use Gcd\Scaffold\Payments\Stripe\Settings\StripeSettings;
 use Gcd\Scaffold\Payments\UI\Entities\PaymentEntity;
+use function GuzzleHttp\Psr7\str;
+use Rhubarb\Leaf\Controls\Common\DateTime\Date;
 use Stripe\Customer;
 use Stripe\Issuing\Card;
 use Stripe\PaymentIntent;
@@ -22,25 +24,26 @@ class StripePaymentService extends PaymentService
 
     public function startPayment(PaymentEntity $entity) : PaymentEntity
      {
-         // Throw an exception if the customerID && cardID isn't supplied OR paymentToken isn't supplied
+         try {
+             $stripeIntent = PaymentIntent::create([
+                 'description' => $entity->description,
+                 'amount' => $entity->amount * 100,
+                 'currency' => $entity->currency,
+                 'confirmation_method' => 'manual',
+                 'payment_method' => $entity->providerPaymentMethodIdentifier,
+                 'capture_method' => ($entity->autoSettle) ? 'automatic' : 'manual'
+             ]);
 
-         $stripeIntent = PaymentIntent::create([
-             'description' => $entity->description,
-             'amount' => $entity->amount * 100,
-             'currency' => $entity->currency,
-             'confirmation_method' => 'manual',
-             'payment_method' => $entity->providerPaymentMethodIdentifier,
-             'capture_method' => ($entity->autoSettle) ? 'automatic' : 'manual'
-         ]);
+             $entity = $this->syncEntity($entity, $stripeIntent);
 
-         // Populate entities
-         $entity->providerIdentifier = $stripeIntent->id;
+             // Extra property used to save a round trip if startPayment and confirmPayment
+             // are called one after the other.
+             $entity->stripeIntent = $stripeIntent;
 
-         // Extra property used to save a round trip if startPayment and confirmPayment
-         // are called one after the other.
-         $entity->stripeIntent = $stripeIntent;
-
-         // Call use case to save payment tracking information for creation
+         } catch (\Exception $er){
+             $entity->status = PaymentEntity::STATUS_FAILED;
+             $entity->error = $er->getMessage();
+         }
 
          return $entity;
      }
@@ -52,6 +55,8 @@ class StripePaymentService extends PaymentService
         } else {
             $stripeIntent = PaymentIntent::retrieve($entity->providerIdentifier);
         }
+
+        $entity = $this->syncEntity($entity, $stripeIntent);
 
         try {
             $stripeIntent->confirm();
@@ -81,5 +86,31 @@ class StripePaymentService extends PaymentService
     public function settlePayment(PaymentEntity $entity): PaymentEntity
     {
         // TODO: Implement settlePayment() method.
+    }
+
+    private function syncEntity(PaymentEntity $entity, $stripeIntent) {
+        // Populate entities
+        $paymentMethod = PaymentMethod::retrieve($stripeIntent->payment_method);
+        $expiry = new \DateTime($paymentMethod->card->exp_year."-".$paymentMethod->card->exp_month."-01");
+
+        $entity->provider = 'Stripe';
+        $entity->providerIdentifier = $stripeIntent->id;
+        $entity->providerPublicIdentifier = $stripeIntent->client_secret;
+        $entity->providerPaymentMethodIdentifier = $stripeIntent->payment_method;
+        $entity->providerPaymentMethodType = $stripeIntent->payment_method_types[0];
+        $entity->status = $stripeIntent->status;
+        $entity->cardExpiry = $expiry->format("m/y");
+        $entity->cardType = $paymentMethod->card->brand;
+        $entity->cardLastFourDigits = $paymentMethod->card->last4;
+
+        $entity->addressCity = isset($entity->addressCity) ? $entity->addressCity : $paymentMethod->billing_details->address->city;
+        $entity->addressLine1 = isset($entity->addressLine1) ? $entity->addressLine1 : $paymentMethod->billing_details->address->line1;
+        $entity->addressLine2 = isset($entity->addressLine2) ? $entity->addressLine2 : $paymentMethod->billing_details->address->line2;
+        $entity->addressPostCode = isset($entity->addressPostCode) ? $entity->addressPostCode : $paymentMethod->billing_details->address->postal_code;
+        $entity->fullName = isset($entity->addressPostCode) ? $entity->addressPostCode : $paymentMethod->billing_details->name;
+        $entity->emailAddress = isset($entity->addressPostCode) ? $entity->addressPostCode : $paymentMethod->billing_details->phone;
+        $entity->phone = isset($entity->addressPostCode) ? $entity->addressPostCode : $paymentMethod->billing_details->email;
+
+        return $entity;
     }
 }
